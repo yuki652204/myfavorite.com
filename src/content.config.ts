6ac6aca6e6fd225354b6c75ca119e50d3ea38762
yuki@ <ookami2204@gmail.com>
2026-04-10 21:27:55 +0900

    initial commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,15 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().optional(),
+      description: z.string().optional(),
+      pubDate: z.coerce.date().optional(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.optional(image()),
+    }),
 });
 
 export const collections = { blog };
